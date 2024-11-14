53786f925ff6f5d57cc458ccbfd21667dd46e104
Miguel O.R. <ma.orjuela73@gmail.com>
2024-11-14 13:46:51 -0600

    Nuevas tuplas y nuevo campo

--- consultas.sql
+++ consultas.sql
@@ -1,10 +1,12 @@
 CREATE TABLE latabla (
 	id SERIAL NOT NULL PRIMARY KEY,
-	nombre VARCHAR(200) NOT NULL
+	nombre VARCHAR(200) NOT NULL,
+	email VARCHAR(200) NOT NULL
 );
 
 INSERT INTO latabla (id, nombre) 
 VALUES 
 	(1,"Awer"), 
-	(2,"Qwerty");
+	(2,"Qwerty")
+	(3,"Zxcvb");
 
